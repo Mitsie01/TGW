6d4545090b872190e9569aa4d1b41f511bd8ccd5
Mitsie01 <mitch.kampert@gmail.com>
2022-09-02 09:28:11 +0200

    updated comments and cleaned up file

--- black_brant_sr/rs_driverfile.m
+++ black_brant_sr/rs_driverfile.m
@@ -1,6 +1,7 @@
-%% Matlab + Simulink rocket simulation
-
-
+%% Matlab + Simulink Black Brant sounding rocket simulation
+% Mitchell Kampert
+% 31-08-2022
+% version 1.1
 
 
 %% Initialisation
@@ -9,6 +10,7 @@ clc
 
 global rad 
 rad = 2*pi/360;                 % From degrees to radians
+
 
 %% Constants
 
@@ -50,11 +52,10 @@ H = 7640;                       % scale height
 % Calculated using Missile DATCOM 98
 
 M=[0.1 0.5 0.8 1 1.2 1.5 2 2.5 3 3.5 4 ....
-    4.5 5 5.5 6 6.5]'; % Mach number range
+    4.5 5 5.5 6 6.5]';                      % Mach number range
 
 Cd=[0.386 0.329 0.325 0.469 0.488 0.434 0.389 0.328 0.287 0.255 0.216 ....
-    0.194 0.184 0.169 0.164 0.154 ]';
-
+    0.194 0.184 0.169 0.164 0.154 ]';       % Drag coeficcient
 
 
 % Wind parameters
@@ -65,8 +66,7 @@ direction = 10*rad;
 var = 5*rad;
 
 
-
-%% Simulink model
+%% Run Simulink and plot data
 
 sim('trajectory_sim.slx')
 

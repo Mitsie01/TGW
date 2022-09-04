ac2c6da8df0e5708ce66fbcdadd77786bf9ab25d
Mitchell Kampert <mitch.kampert@gmail.com>
2022-09-05 00:21:19 +0200

    added centripetal force module and made wind optional boolean value

--- black_brant_sr/rs_driverfile.m
+++ black_brant_sr/rs_driverfile.m
@@ -16,7 +16,7 @@ rad = 2*pi/360;                 % From degrees to radians
 
 % generic constants
 G = 6.67430*10^-11;             % Gravitational constant
-dt = 0.001;                     % simulation time step
+dt = 0.01;                     % simulation time step
 
 % launch configuration
 
@@ -26,7 +26,7 @@ y0 = 0;                         % y coordinate
 z0 = 0;                         % z coordinate
 
 hdg = 0*rad;                    % heading
-pitch = 90*rad;                 % pitch
+pitch = 30*rad;                 % pitch
 
 v0 = 0;                         % starting velocity
 
@@ -47,6 +47,8 @@ D0 = 1.225;                     % density at sealevel (kg/m^3)
 P0 = 1.01325*10^5;              % pressure at sealevel (Pa)
 H = 7640;                       % scale height
 
+centrifugal_force = true;       % enable/disable centrifugal force
+
 
 
 % Calculated using Missile DATCOM 98
@@ -60,9 +62,11 @@ Cd=[0.386 0.329 0.325 0.469 0.488 0.434 0.389 0.328 0.287 0.255 0.216 ....
 
 % Wind parameters
 
+wind_effect = true;
+
 vmin = 10;
 vmax = 15;
-direction = 10*rad;
+direction = 40*rad;
 var = 5*rad;
 
 
@@ -78,6 +82,14 @@ xlabel('Longitudinal distance (m)')
 ylabel('Lateral distance (m)')
 zlabel('Altitude (m)')
 
+figure('Name','Total velocity','NumberTitle','off')
+title('Total velocity')
+hold on
+xlabel('Time (s)') 
+ylabel('Velocity (m/s)')
+plot(t,v)
+hold off
+
 figure('Name','Thrust','NumberTitle','off')
 title('Thrust')
 hold on
@@ -92,6 +104,26 @@ hold on
 xlabel('Time (s)') 
 ylabel('Mass (kg)')
 plot(t,mass)
+hold off
+
+figure('Name','Air density','NumberTitle','off')
+title('Air density')
+hold on
+xlabel('Time (s)') 
+ylabel('Density (kg/m3)')
+plot(t,density)
+hold off
+
+figure('Name','Vertical accelleration','NumberTitle','off')
+title('vertical accelleration')
+hold on
+xlabel('Time (s)') 
+ylabel('Force (N)')
+
+plot(t,fc)
+plot(t,fg)
+
+legend({'Centrifugal force','Gravitational force'},'Location','southwest')
 hold off
 
 figure('Name','Drag force','NumberTitle','off')

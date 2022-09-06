42c47e14a6019102cf5303336e84177ba3eb5daf
Mitchell Kampert <mitch.kampert@gmail.com>
2022-09-06 10:03:23 +0200

    new centrifugal model

--- black_brant_sr/globe/globe_test.m
+++ black_brant_sr/globe/globe_test.m
@@ -7,12 +7,15 @@ rad = 2*pi/360;                                             % From degrees to ra
 
 %% Constants
 
+%general constants
+dt = 0.01;
+
 %Get Planet Parameters
 planet
 
 %Initial Conditions
 launch_altitude = 100;                                        % altitude of launching platform (m)
-V0 = 7000;
+V0 = 7100;
 
 
 r0 = r_earth+launch_altitude;                                % distance from the center of Earth (m)
@@ -32,8 +35,9 @@ Z = Z*r_earth/1000;
 
 
 %Plot 3D orbit
+sim('globe_simulator.slx')
 
-[x,y,z] = sph2cart(theta0, phi0, r0);
+[x,y,z] = sph2cart(theta, phi, r);
 
 fig = figure();
 set(fig,'color','white')
@@ -41,7 +45,23 @@ plot3(x,y,z,'b-','LineWidth',4)
 grid on
 hold on
 %surf(X,Y,Z,'EdgeColor','none')
-axis equal
+%axis equal
 xlabel('x') 
 ylabel('y')
 zlabel('z')
+
+figure('Name','Altitude','NumberTitle','off')
+title('Altitude')
+hold on
+xlabel('Time (s)') 
+ylabel('Altitude (m)')
+plot(t,r-r_earth)
+hold off
+
+figure('Name','Accelleration','NumberTitle','off')
+title('Accelleration')
+hold on
+xlabel('Time (s)') 
+ylabel('Acceleration to center (m/s^2)')
+plot(t,g)
+hold off

673d81ee7d411e5d08af4d63bed8dfe568c505f1
Mitsie01 <mitch.kampert@gmail.com>
2022-11-04 12:49:17 +0100

    created function for cn and cm

--- sparrow/main.m
+++ sparrow/main.m
@@ -9,17 +9,43 @@
 global rad 
 rad = 2*pi/360;                 % From degrees to radians
 
-% timestep settings
+% sim settings
 t0 = 0;
 dt = 0.1;
 tmax = 1000;
+alpha_0 = 0;
+alpha_max = 19;
+d_alpha = 1;
+Ma = 2;
+delta = 0;
 
 % load data
 sparrow
 earth
+datcom_data
 
 %% run simulation
 
-alpha = 0;
+[CN_sim, CM_sim, alpha_sim] = Cnorm(alpha_0, d_alpha, alpha_max, Ma, delta, Splan, Sw, St, Sref);
 
-Cn = 2*alpha*()
+%% plot graphs
+
+figure('Name','Cn','NumberTitle','off')
+title('Cn')
+hold on
+plot(alpha_datcom, CN_datcom)
+plot(alpha_sim, CN_sim)
+grid on
+xlabel('alpha') 
+ylabel('Cn')
+hold off
+
+figure('Name','Cm','NumberTitle','off')
+title('Cm')
+hold on
+plot(alpha_datcom, CM_datcom)
+plot(alpha_sim, CM_sim/1000)
+grid on
+xlabel('alpha') 
+ylabel('Cm')
+hold off
--- sparrow/sparrow.m
+++ sparrow/sparrow.m
@@ -9,6 +9,8 @@ d = 0.203;
 
 Xw = 1.102;
 
+Xcg = 1.925;
+
 Crw = 0.495;
 Ctw = 0.097;
 Hw = 0.398;
@@ -17,5 +19,19 @@ Crt = 0.473;
 Ctt = 0;
 Ht = 0.308;
 
+An = 0.67*Ln*d;
+Ab = (L-Ln)*d;
+
+%% Centers of pressure
+
+Xcpn = 0.67*Ln;
+Xcpb = ((0.67*An*Ln)+Ab*(Ln+0.5*(L-Ln)))/(An+Ab);
 Xhl = 1.829;
-Xcg = 1.925;
+Xcpt = L-0.3*Crt-0.2*Ctt;
+
+%% S values
+
+Sref = (pi*d^2)/4;
+Sw = 0.5*Hw*(Ctw+Crw);
+St = 0.5*Ht*(Ctt+Crt);
+Splan = ((L-Ln)*d)+(0.67*Ln*d);
